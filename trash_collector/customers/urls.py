6c785731a2cbb68667768b81f383c6d59af8ee6e
Macm91 <macm91.mc@gmail.com>
2021-08-30 18:35:45 -0400

    fixed capitalization issue with pickup day in create.html by changing it to a drop down option

--- trash_collector/customers/urls.py
+++ trash_collector/customers/urls.py
@@ -10,15 +10,7 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('new/',views.create, name = 'create'),
     path('update_pickup/', views.update_pickup, name = 'update_pickup'),
-<<<<<<< HEAD
-    path('suspend_pickup/', views.suspend_pickup, name = 'suspend_pickup'),
-    path('balance/', views.account_balance, name= 'accound_balance')
-=======
-<<<<<<< HEAD
     path('one_time_pickup/', views.one_time_pickup, name = 'one_time_pickup'),
-=======
->>>>>>> ef529b13f98681e5f9bda4d1110b4edd9a904dee
-    path('suspend_pickup/', views.suspend_pickup, name = 'suspend_pickup')
->>>>>>> 1d7dbb1787081f45f89a3aef60119e0f891c1c1a
+    path('suspend_pickup/', views.suspend_pickup, name = 'suspend_pickup'),
+    path('balance/', views.account_balance, name= 'account_balance')
 ]
-
